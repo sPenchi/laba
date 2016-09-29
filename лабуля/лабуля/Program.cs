89e8abe86e89f13e518cbbd79c812528a3462727
sPenchi <mr.pronenko@bk.ru>
2016-09-29 15:23:46 +0300

    int result = a + b

--- лабуля/лабуля/Program.cs
+++ лабуля/лабуля/Program.cs
@@ -11,6 +11,13 @@ namespace лабуля
         static void Main(string[] args)
         {
             System.Console.WriteLine("Привет мир!");
+            int a = 5;
+            int b = 2;
+            System.Console.WriteLine("a =  " + a + ", b = " + b);
+
+            int result = a + b;
+            System.Console.WriteLine("Сложение, a + b =  " + result);
+
             System.Console.ReadLine();
 
         }
