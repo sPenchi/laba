900fc94cb35939c9ba1f5129fa07384e27462584
sPenchi <mr.pronenko@bk.ru>
2016-09-29 15:21:55 +0300

    hello

---
+++ лабуля/лабуля/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лабуля
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Привет мир!");
+            System.Console.ReadLine();
+
+        }
+    }
+}
